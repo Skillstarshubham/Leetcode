42d4afb70d46a2468eec137ef5e9121b1188f131
Skillstarshubham <105667145+Skillstarshubham@users.noreply.github.com>
2024-06-20 10:35:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (54.30%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -3,13 +3,10 @@ public:
     int hIndex(vector<int>& c) {
         sort(c.begin(), c.end());
         int n = c.size();
-        int maxi = 0;
         for(int i = 0; i < n; i++) {
-            if(c[i] >= n - i) {
-                maxi = max(maxi, n - i);
-                break;
-            }
+            if(c[i] >= n - i)
+                return n - i;
         }
-        return maxi;
+        return 0;
     }
 };
