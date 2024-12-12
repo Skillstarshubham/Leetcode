631d93d44db81d196b323ce5f5d211f62dd3cbaa
Skillstarshubham <105667145+Skillstarshubham@users.noreply.github.com>
2024-12-12 17:50:10 +0530

    Time: 7 ms (18.03%) | Memory: 9.3 MB (61.63%) - LeetSync

---
+++ 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        for(int i=0;i<longUrl.size();i++){
+            longUrl[i]=char(int(longUrl[i])+1);
+        }
+        return longUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        for(int i=0;i<shortUrl.size();i++){
+            shortUrl[i]=char(int(shortUrl[i])-1);
+        }
+        return shortUrl;
+    }
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
