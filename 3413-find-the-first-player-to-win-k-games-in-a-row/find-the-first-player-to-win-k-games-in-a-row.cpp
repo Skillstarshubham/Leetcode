fce609bb9b2ad3795773290dc01c22fbe5b5cdad
Shubham Kumar <105667145+Skillstarshubham@users.noreply.github.com>
2025-10-16 08:13:23 +0530

    Time: 0 ms (100.00%) | Memory: 120.8 MB (57.31%) - LeetSync

--- 3413-find-the-first-player-to-win-k-games-in-a-row/find-the-first-player-to-win-k-games-in-a-row.cpp
+++ 3413-find-the-first-player-to-win-k-games-in-a-row/find-the-first-player-to-win-k-games-in-a-row.cpp
@@ -1,37 +1,14 @@
 class Solution {
 public:
-    int findWinningPlayer(vector<int>& skills, int k) {
-        int c=0;
-        if(k>=skills.size()){
-            int ans=0;
-            int maxi=0;
-            for(int i=0;i<skills.size();i++){
-                if(skills[i]>maxi){
-                    maxi=skills[i];
-                    ans=i;
-                }
+        int findWinningPlayer(vector<int>& A, int k) {
+        int i = 0, cur = 0, n = A.size();
+        for (int j = 1; j < n; ++j) {
+            if (A[i] < A[j]) {
+                cur = 0;
+                i = j;
             }
-            return ans;
+            if (++cur == k) break;
         }
-        for(int i=0;i<skills.size();i++){
-            for(int j=i+1;j<skills.size();j++){
-                if(skills[i]>skills[j]){
-                c++;
-                if(c==k)
-                return i;
-                skills.push_back(skills[j]);
-                }
-                else{
-                c=1;
-                if(c==k)
-                return j;
-                skills.push_back(skills[i]);
-                i=j-1;
-                break;
-                }
-                
-            }
-        }
-        return 0;
+        return i;
     }
 };
